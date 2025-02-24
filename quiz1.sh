755f354440f9bb4f6ff5c44ebbbc8d88b09608c9
Emmanuel Odenyire <odenyireemmanuel@gmail.com>
2025-02-24 23:28:30 +0300

    Update quiz1.sh

--- quiz1.sh
+++ quiz1.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-answer=$1
+
+echo "Which of the following best describes software engineering?"
+echo "a) The study of hardware components"
+echo "b) The process of designing, developing, and maintaining software"
+echo "c) The analysis of networking protocols"
+echo "d) The management of data storage systems"
+
+read answer
+
 if [ "$answer" == "b" ]; then
     echo "Correct"
     exit 0
